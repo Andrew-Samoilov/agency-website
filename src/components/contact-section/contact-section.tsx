ce227727c691ec5eae626f631b826e5cea9bb3a0
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-01-07 18:04:28 +0200

    add: contact us section

--- src/components/contact-section/contact-section.tsx
+++ src/components/contact-section/contact-section.tsx
@@ -2,15 +2,16 @@ import Link from "next/link";
 
 export default function ContactSection() {
 	return (
-    <section className="bg-emerald-100">
-      <div className="flex justify-center items-center">
-        <h2>
-          Have project in mind?
-          <br />
-          Lete&apos;s discuss
+    <section className="bg-gray-100">
+      <div className="flex items-center flex-col md:flex-row">
+        <h2 className="leading-relaxed font-bold text-3xl md:text-5xl md:leading-relaxed md:max-w-3xl p-[0.75em] md:pl-[1em] lg:pl-[3em]">
+          Have project in mind? Let&apos;s discuss
         </h2>
-        <Link href="/contact" className="ml-2">
-          Contact Us
+        <Link
+          href="/contact"
+          className="bg-sky-700 text-white font-semibold px-[1em] py-[0.5em] md:mr-10 lg:ml-20 mb-6 md:mb-0 hover:text-sky-700 hover:bg-white border border-sky-700"
+        >
+          Contact&nbsp;Us
         </Link>
       </div>
     </section>
--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
       <h1 className="text-7xl md:text-9xl lg:text-[192px] pt-[0.75em] font-bold tracking-wide text-sky-700 pl-4 md:pl-[0.75em] mb-[-0.1em]">
         Hello &mdash;
       </h1>
-      <div className="text-3xl md:text-5xl font-bold  bg-sky-700 text-white md:pr-[2em] ">
+      <div className="text-3xl md:text-5xl font-bold bg-sky-700 text-white md:pr-[2em] ">
         <p className="p-[0.75em] pt-[1.5em] leading-relaxed md:max-w-6xl md:pl-[1em] lg:pl-[3em]">
           Welcome to our world. We&apos;re Tech Vibe,{" "}
           <span className="text-sky-700 font-outline hover:text-white hover:font-outline-none">
@@ -16,8 +16,8 @@ export default function HeroSection() {
           We help our clients start, sustain, and grow their business.
         </p>
         <p className="p-[0.75em] leading-relaxed md:max-w-6xl md:pl-[1em] lg:pl-[3em]">
-          Want to run through the details of your next big project, Lete&apos;s
-          chat now.
+          Whether you&apos;ve got a quick concern or want to run through the
+          details of your next big project, we can help. Lete&apos;s chat now.
         </p>
       </div>
     </section>
