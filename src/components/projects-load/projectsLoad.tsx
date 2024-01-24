cf1d470145bd8dbc1bfe20e7b356ec1dd41bd0c5
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-01-24 15:02:42 +0100

    Add: News description on hover

--- src/components/projects-load/projectsLoad.tsx
+++ src/components/projects-load/projectsLoad.tsx
@@ -8,28 +8,32 @@ export default function ProjectsLoad({
   return (
     <>
       {projectsData.map((project) => (
-        <article className="md:p-[1em] group" key={project.id}>
-          <Link href={isMain ? "/projects" : `/projects/${project.id}`}>
-            <h3 className="text-2xl md:text-5xl lg:text-6xl pb-[0.25em]">
+        <article className="relative md:p-[1em]" key={project.id}>
+          <Link
+            href={isMain ? "/projects" : `/projects/${project.id}`}
+            className="group z-0"
+          >
+            <h3 className="text-center text-2xl md:text-5xl lg:text-6xl pb-[0.25em] h-[2em]">
               {project.name}
-            </h3>{" "}
+            </h3>
+            {project.imgUrl && (
+              <Image
+                src={project.imgUrl}
+                alt={project.title}
+                width={300}
+                height={200}
+              ></Image>
+            )}
+            <p className=" pt-[0.75em] pb-[0.25em]">{project.title}</p>
+            <div className="absolute md:p-[1em] top-0 left-0 w-full h-full z-10 opacity-0 group-hover:opacity-100 bg-main-sky/90 text-white">
+              <p className="text-center text-2xl md:text-5xl lg:text-6xl pb-[0.25em]">
+                {project.name}
+              </p>
+              <p className="p-[2em] leading-normal">{project.description}</p>
+              <p className="p-[2em]">More...</p>
+            </div>
           </Link>
-          {project.imgUrl && (
-            <Image
-              src={project.imgUrl}
-              alt={project.title}
-              width={300}
-              height={200}
-            ></Image>
-          )}
-          <p className="font-bold pt-[0.75em] pb-[0.25em]">{project.title}</p>
-          {/* <p className="pb-[0.5em] hidden lg:block">{project.description}</p> */}
-          <p className="pb-[0.5em] opacity-0 group-hover:opacity-100">{project.description}</p>
-          {!isMain && (
-            <Link href={project.url} target="blank">
-              {project.urlTitle}
-            </Link>
-          )}
+          <hr className="md:hidden" />
         </article>
       ))}
     </>
