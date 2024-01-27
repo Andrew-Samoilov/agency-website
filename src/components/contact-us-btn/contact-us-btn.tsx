64e28cb83c471e2e8e4c1d49eef53cfd070ed788
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-01-27 11:19:56 +0100

    add: contact us button hover

--- src/components/contact-us-btn/contact-us-btn.tsx
+++ src/components/contact-us-btn/contact-us-btn.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
 
 export default function ContactUsBtn({ buttonLook }: Readonly<{ buttonLook?: string }>) {
-  let buttonStyle = "bg-main-sky dark:bg-sky-300 text-white dark:text-black";
-  switch (buttonLook) {
-      case "main":
-        buttonStyle = "bg-white dark:bg-black text-main-sky dark:text-sky-300";
-        break;
-      case "red":
-        buttonStyle = "bg-red";
-        break;
-      default:
-        break;
-    }
+    let buttonStyle =
+      "border-main-sky bg-main-sky dark:bg-sky-300 text-white dark:text-black hover:bg-white hover:text-main-sky duration-300";
+    switch (buttonLook) {
+        case "main":
+            buttonStyle =
+            "bg-white hover:bg-main-sky dark:bg-black text-main-sky hover:text-white dark:text-sky-300 border-main-sky hover:border-white duration-300";
+            break;
+        case "red":
+            buttonStyle = "bg-red";
+            break;
+        default:
+            break;
+}
   
   return (
     <Link
-      href="/contact"
-      className={` font-semibold px-[1em] py-[0.5em] hover:text-main-sky hover:bg-white border border-main-sky ${buttonStyle}`}
-    >
-      Contact&nbsp;Us
+        href="/contact"
+        className={`font-semibold px-[1em] py-[0.5em] border ${buttonStyle}`}
+        >
+        Contact&nbsp;Us
     </Link>
   );
 }
