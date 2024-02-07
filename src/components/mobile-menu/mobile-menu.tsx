490b5a25531afac40395108d5ee968f1477a982b
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-02-07 22:15:54 +0200

    add: mobile menu

---
+++ src/components/mobile-menu/mobile-menu.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import React, { useState } from 'react';
+import MainMenu from '../main-menu/mainMenu';
+import Logo from '../logo/logo';
+
+
+export default function MobileMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+        // for stop scrollin when isOpen
+        const html = document.querySelector('html');
+        !isOpen ? html!.style.overflow = 'hidden' : html!.style.overflow = 'auto';
+    };
+
+    return (
+        <>
+            <button onClick={handleClick}
+                aria-label="Open mobile menu"
+                className='flex flex-col justify-center items-center md:hidden mr-2 z-30'>
+                <span className={`bg-main-sky transition-all duration-300 ease-out 
+                h-1 w-6 rounded-sm ${isOpen ?
+                        'rotate-45 translate-y-1.5' : '-translate-y-1'
+                    }`} >
+                </span>
+                <span className={`bg-main-sky transition-all duration-300 ease-out 
+                h-1 w-6 rounded-sm my-0.5 ${isOpen ?
+                        'opacity-0' : 'opacity-100'
+                    }`} >
+                </span>
+                <span className={`bg-main-sky transition-all duration-300 ease-out 
+                h-1 w-6 rounded-sm ${isOpen ?
+                        '-rotate-45 -translate-y-1.5' : 'translate-y-1'
+                    }`} >
+                </span>
+            </button>
+
+            {
+                isOpen ? (
+                    <div className="z-20 fixed top-0 left-0 w-screen h-[100vh] p-6 flex flex-col justify-between items-center bg-white dark:bg-black">
+                        <Logo
+                            styleLogo='mr-auto'
+                            onClickFunction={handleClick}
+                        /> 
+                        <hr className="rounded-sm  w-full h-1 border-t-0 bg-main-sky opacity-100 dark:opacity-30" />
+                        <MainMenu
+                            styleMenuItem="list-none text-xl"
+                            styleNav="grid grid-rows-2 gap-8"
+                            onClickFunction={handleClick}
+                        />
+                        <hr className=" rounded-sm w-full h-1 border-t-0 bg-main-sky opacity-100 dark:opacity-30" />
+                  
+                    </div>
+                ) : (<></>)
+            }
+        </>)
+}
