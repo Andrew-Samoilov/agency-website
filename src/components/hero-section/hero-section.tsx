268e1ba11b1aed1ae5a20e6abe02772bc38e3930
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-02-15 00:08:43 +0200

    fix: mobile menu

--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function HeroSection() {
     return (
         <section className="">
-            <h1 className="text-7xl md:text-9xl lg:text-[192px] pt-[0.75em] font-bold tracking-wide text-main-sky  pl-4 md:pl-[0.75em] mb-[-0.125em]">
+            <h1 className="text-7xl md:text-9xl lg:text-[192px] pt-[0.75em] font-bold tracking-wide text-main-sky  pl-4 md:pl-[0.75em] mb-[-0.13em]">
                 Hello &mdash;
             </h1>
-            <div className="relative text-3xl md:text-5xl font-bold bg-main-sky  text-white dark:text-black md:pr-[2em] z-0">
+            <div className="relative text-3xl md:text-5xl font-bold bg-main-sky  text-white dark:text-black md:pr-[2em] z-0 ">
                 <Image
                     src={"/images/bg-drop.svg"}
                     alt="animated backgound"
@@ -17,14 +17,16 @@ export default function HeroSection() {
                 />
                 <p className="p-[0.75em] pt-[1.5em] leading-relaxed md:max-w-6xl md:pl-[1em] lg:pl-[3em]">
                     Welcome to our world. We&apos;re Tech Vibe,{" "}
-                    <span className="text-transparent font-outline-white dark:font-outline-black hover:text-white dark:hover:text-sky-300 hover:font-outline-none">
+                    <span
+                        className="md:text-transparent md:font-outline-white md:dark:font-outline-black md:hover:text-white md:dark:hover:text-sky-300 md:hover:font-outline-none">
                         a creative digital agency.
                     </span>{" "}
                     We offer various types of digital services.
                 </p>
                 <p className="p-[0.75em] leading-relaxed md:max-w-6xl md:pl-[1em] lg:pl-[3em]">
                     We help{" "}
-                    <Link href="/projects" className="md:text-transparent md:font-outline-white md:dark:font-outline-black md:hover:text-white md:dark:hover:text-sky-300 md:hover:font-outline-none">our clients
+                    <Link href="/projects"
+                        className="md:text-transparent md:font-outline-white md:dark:font-outline-black md:hover:text-white md:dark:hover:text-sky-300 md:hover:font-outline-none">our clients
                     </Link>{" "}start, sustain, and grow their business.
                 </p>
                 <p className="p-[0.75em] leading-relaxed md:max-w-6xl md:pl-[1em] lg:pl-[3em]">
