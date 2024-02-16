4cb9859c47ceed843d4cd3c357631fe9038eeeed
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-02-16 21:25:10 +0200

    add: stages section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Blockquote from "@/components/blockquote/blockquote";
 import ContactSection from "@/components/contact-section/contact-section";
 import HeroSection from "@/components/hero-section/hero-section";
 import ProjectsSection from "@/components/projects-section/projects-section";
+import Stages from "@/components/stages/stages";
 import TeamSection from "@/components/team-section/team-section";
 import TechCloudSection from "@/components/tech-cloud-section/tech-cloud-section";
 
@@ -13,11 +14,12 @@ export default function Home() {
         <Blockquote
           text="The only way to do great work is to love what you do."
           author="Steve Jobs"
-          className="text-3xl md:text-5xl  max-w-3xl md:pr-[2em] text-slate-900 dark:text-slate-100"
+          className="text-3xl md:text-5xl  max-w-3xl md:pr-[2em]"
         />
       </section>
       <hr />
       <ProjectsSection />
+      <Stages />
       <TechCloudSection />
       <TeamSection />
       <ContactSection sectionLook="main" />
---
+++ src/components/stages/stages-data.ts
@@ -0,0 +1,17 @@
+export const stagesData = [
+    {
+        id: "01",
+        name: "Research and analytics",
+        text: "We start with research and analytics.",
+    },
+    {
+        id: "02",
+        name: "Design and Develop",
+        text: "In this step, we сreate design paired with detailed development for innovative solutions.",
+    },
+    {
+        id: "03",
+        name: "Testing and Launch",
+        text: "The final step of success is testing and launch process to successful results.",
+    },
+]
