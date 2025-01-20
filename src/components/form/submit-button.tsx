d667dd0d2ca33c13d8f59e83eedf8b40e57189d2
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-01-20 13:50:05 +0200

    refactor: remove react.fc

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -1,20 +1,21 @@
 "use client";
 
+import { ReactNode } from "react";
 import { useFormStatus } from "react-dom";
 
 interface SubmitButtonProps {
   pendingText?: string;
   className?: string;
-  children: React.ReactNode;
+  children: ReactNode;
   disabled?: boolean;
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({
-  pendingText = "Надсилання...",
-  className,
+const SubmitButton = ({
+  pendingText = "Sending...",
+  className = "",
   children,
   disabled = false,
-}) => {
+}: SubmitButtonProps) => {
   const { pending } = useFormStatus();
 
   return (
